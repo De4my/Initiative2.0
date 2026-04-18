01a6a7f6fef58ef5b1702d7bb38a008d1b9c1239
De4my <amohdyasin2015@gmail.com>
2026-04-19 07:42:56 +0800

    fgege

--- newVis.py
+++ newVis.py
@@ -13,7 +13,7 @@ load_dotenv()
 df = pd.read_excel("RouteA_Final.xlsx", sheet_name="Route_Coordinates")
 df2 = pd.read_excel("RouteA_Final.xlsx", sheet_name="Bus_Stops")
 
-
+#HELLLLLLLLLLLLLLLLLLLLLLLLLLo
 # Clean route dataframe
 df.columns = df.columns.str.strip().str.lower()
 df = df.rename(columns={"latitude": "lat", "longitude": "lon"})
